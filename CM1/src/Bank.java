c14d250b467102328f83ea4b1d619c87b1c7dfb7
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2025-04-16 09:25:56 +0700

    bank

--- CM1/src/Bank.java
+++ CM1/src/Bank.java
@@ -1,4 +1,17 @@
 public class Bank {
-
-    
+    String noRekening, nama, namaIbu, noHp, email;
+    Bank(String norek, String nm, String nmIbu, String no, String eml){
+        this.noRekening = noRekening;
+        this.nama = nama;
+        this.namaIbu = namaIbu;
+        this.noHp = noHp;
+        this.email = email;
+    }
+    void tampiDataNorek(){
+        System.out.println("No Rekening: " + noRekening);
+        System.out.println("Nama       : " + nama);
+        System.out.println("Nama Ibu   : " + namaIbu);
+        System.out.println("No HP      : " + noHp);
+        System.out.println("Email      : " + email);
+    }
 }
