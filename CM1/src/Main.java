e16bd0ef633d4498ee10dbfb8d9b7e0b73d57b2f
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2025-04-16 10:44:55 +0700

    push lagi

--- CM1/src/Main.java
+++ CM1/src/Main.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ServiceTransaksi list = new ServiceTransaksi();
+
         Bank[] arrBank = new Bank[5]; 
+        list.ServiceTransaksi(arrBank.length);
         arrBank[0] = new Bank("1634823 3084", "Wallace", "Mei Mei", "0829-3452-8477", "wallace@gmail.com");
         arrBank[1] = new Bank("1624638 4364", "Darius", "Susanti", "0823-4435-4427", "darius@gmail.com");
         arrBank[2] = new Bank("1633425 5234", "Fuller", "Rosalia", "0837-7463-7283", "fuller@gmail.com");
@@ -11,8 +14,15 @@ public class Main {
         arrBank[4] = new Bank("1625634 1344", "Gery", "Fatimah", "0859-3123-7727", "gery@gmail.com");
 
         Transaksi[] arrTransaksi = new Transaksi[5];
-        arrTransaksi[0] = new Transaksi(null, 0, 0, 0, null, null);
+        arrTransaksi[0] = new Transaksi("Tr005", 500000, 200000, 700000, "23-04-2025", "Kredit");
+        arrTransaksi[1] = new Transaksi("Tr004", 400000, 150000, 650000, "23-04-2025", "Kredit");
+        arrTransaksi[2] = new Transaksi("Tr003", 300000, 100000, 200000, "23-04-2025", "Debit");
+        arrTransaksi[3] = new Transaksi("Tr002", 200000, 200000, 400000, "23-04-2025", "Kredit");
+        arrTransaksi[4] = new Transaksi("Tr001", 100000, 50000, 50000, "23-04-2025", "Debit");
 
+        for (int i = 0; i < arrBank.length; i++) {
+            list.TampilData(arrTransaksi[i]);
+        }
 
         while (true) {
             System.out.println("===== POLINEMA BANK =====");
@@ -28,7 +38,7 @@ public class Main {
             int pilih = sc.nextInt();
             switch (pilih) {
                 case 1:
-                    
+                    list.TampilData(arrBank);
                     break;
                 case 2:
                     
